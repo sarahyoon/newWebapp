c1ff31efda967e606b9a3369c44b47fac28ab96a
sarahyoon <yoonsarah88@gmail.com>
2023-10-24 15:25:41 +0900

    new project

---
+++ src/main/java/com/example/newwebapp/NewWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.newwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newwebapp/NewWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
